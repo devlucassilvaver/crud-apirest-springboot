8a73d93b12c2ec0d495ea20d885589183e6894fe
LK <lorenakampslk@gmail.com>
2026-09-02 20:14:19 -0300

    feat: adiciona limite para paginação de produtos

---
+++ api-rest/src/main/java/com/lucassilva/api_rest/config/PageableConfig.java
@@ -0,0 +1,19 @@
+package com.lucassilva.api_rest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class PageableConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
+        PageableHandlerMethodArgumentResolver pageableHandler = new PageableHandlerMethodArgumentResolver();
+
+        pageableHandler.setMaxPageSize(50);
+        resolvers.add(pageableHandler);
+    }
+}
